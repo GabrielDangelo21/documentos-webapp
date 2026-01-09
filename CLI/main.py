f65118aa78551e0a012f3bfb80a1555da5da2584
GabrielDangelo21 <g.dangelo0408@gmail.com>
2026-01-09 23:29:38 +0100

    Atualização features edição e remoção

--- CLI/main.py
+++ CLI/main.py
@@ -3,16 +3,6 @@ from typing import List
 import json
 from operator import itemgetter
 
-"""
- TODO
-Remover documento por nome
-Editar validade/alerta
-Ordenar por vencimento
-Buscar por nome
-Enviar alerta de alguma forma
-"""
-
-
 def ler_data(prompt: str) -> datetime.date:
     while True:
         try:
@@ -41,8 +31,8 @@ def ler_int(prompt: str) -> int:
 def cadastrar_documento(documentos: List[dict], nome: str, data_validade: date, alerta_dias: int) -> None:
     if nome.strip() == "":
         raise ValueError('Digite o nome do documento.')
-    if alerta_dias < data_validade:
-        raise ValueError('O documento já está vencido.')
+    if alerta_dias < 0:
+        raise ValueError('Dias para alerta não pode ser negativo.')
     documento = {'nome':nome, 'validade':data_validade, 'alerta':alerta_dias}
     documentos.append(documento)
 
@@ -67,15 +57,24 @@ def listar_documentos(documentos: list[dict]) -> None:
     documentos_ordenados = sorted(documentos, key=itemgetter('validade', 'nome'))
     for documento in documentos_ordenados:
         nome = documento['nome']
+        validade = documento['validade']
         dias, status = calcular_status(documento)
+        if dias < 0:
+            print(f'{nome} | validade {validade} | vencido há {-(dias)} dia(s) | {status}')
+        else:    
+            print(f'{nome} | validade {validade} | vence em {dias} dia(s) | {status}')
 
-        print(f'{nome} | vence em {dias} dia(s) | {status}')
-
-def listar_documentos_com_indice(documentos: List[dict]):
-    for indice, documento in enumerate(documentos, start=1):
+def listar_documentos_com_indice(documentos: List[dict]) -> List[dict]:
+    documentos_ordenados = sorted(documentos, key=itemgetter('validade', 'nome'))
+    for indice, documento in enumerate(documentos_ordenados, start=1):
         nome = documento['nome']
+        validade = documento['validade']
         dias, status = calcular_status(documento)
-        print(f'{indice}) {nome} | vence em {dias} dia(s) | {status}')
+        if dias < 0:
+            print(f'{indice}) {nome} | validade {validade} | vencido há {-(dias)} dia(s) | {status}')
+        else:    
+            print(f'{indice}) {nome} | validade {validade} | vence em {dias} dia(s) | {status}')
+    return documentos_ordenados
 
 def listar_alertas(documentos: list[dict]) -> None:
     if not documentos:
@@ -85,9 +84,11 @@ def listar_alertas(documentos: list[dict]) -> None:
     for documento in documentos:
         nome = documento['nome']
         dias, status = calcular_status(documento)
-        if status == "Vencido" or status == "Alerta":
-            print(f'{nome} | {status} | vence em {dias} dia(s).')
-            alerta_encontrado = True
+        if dias < 0:
+            print(f'{nome} | vencido há {-(dias)} dia(s)| {status}')
+        else:    
+            print(f'{nome} | vence em {dias} dia(s) | {status}')
+        alerta_encontrado = True
     if not alerta_encontrado:
         print('Nenhum documento em alerta.')
 
@@ -113,7 +114,7 @@ def salvar_documento(documentos: List[dict], caminho: str) -> None:
         json.dump(documentos_json, f, indent=2)
 
 def carregar_documentos(caminho: str) -> List[dict]:
-    Documentos = []
+    documentos = []
     try:
         with open(caminho, "r", encoding="utf-8") as f:
             documento = json.load(f)
@@ -124,8 +125,8 @@ def carregar_documentos(caminho: str) -> List[dict]:
                 validade = datetime.strptime(data_validade, data_format).date()
                 alerta = dados['alerta']
                 doc = {'nome':nome, 'validade': validade, 'alerta': alerta}
-                Documentos.append(doc)
-            return Documentos
+                documentos.append(doc)
+            return documentos
     except FileNotFoundError:
         return []
         
@@ -169,19 +170,30 @@ def main() -> None:
                 print(f'Erro: {e}')
 
         elif opcao == 2:
+            print('=================================')
             listar_documentos(documentos)
+            print('=================================')
 
         elif opcao == 3:
+            print('=================================')
             listar_alertas(documentos)
+            print('=================================')
+
 
         elif opcao == 4:
             try:
-                termo = ler_str('Digite o nome: ')
+                termo = ler_str('Digite o nome do documento: ')
                 documentos_encontrados = buscar_documentos(documentos, termo)
                 if not documentos_encontrados:
+                    print('=================================')
                     print('Nenhum documento encontrado')
+                    print('=================================')
+
                 else:
+                    print('=================================')
                     listar_documentos(documentos_encontrados)
+                    print('=================================')
+
             except ValueError as e:
                 print(f'Erro: {e}')
 
@@ -189,89 +201,136 @@ def main() -> None:
             termo = ler_str('Qual documento deseja remover? ')
             resultados = buscar_documentos(documentos, termo)
             if not resultados:
+                print('=================================')
                 print('Nenhum documento encontrado.')
+                print('=================================')
                 continue
             else:
-                listar_documentos_com_indice(resultados)
-                indice = ler_int('Digite o número do documento que deseja remover: ')
-                if indice < 1 or indice > len(resultados):
-                    print('Número de documento inválido.')
-                else:
-                    doc_escolhido = resultados[indice - 1]
-                    documentos.remove(doc_escolhido)
-                    salvar_documento(documentos, caminho)
-                    print('Documento removido com sucesso.')
+                print('=================================')
+                resultados_ordenados = listar_documentos_com_indice(resultados)
+                print('=================================')
+                while True:
+                    indice = ler_int('Digite o número do documento que deseja remover: ')
+                    if indice < 1 or indice > len(resultados_ordenados):
+                        print('=================================')
+                        print('Número de documento inválido.')
+                        print('=================================')
+                        continue
+
+                    doc_escolhido = resultados_ordenados[indice - 1]
+                    
+                    while True:
+                        opcao_confirmacao = ler_str('Deseja mesmo remover o documento? (s/n)').strip().lower()
+                        if opcao_confirmacao == 's':
+                            documentos.remove(doc_escolhido)
+                            salvar_documento(documentos, caminho)
+                            print('=================================')
+                            print('Documento removido com sucesso.')
+                            print('=================================')
+                            break
+                        elif opcao_confirmacao == 'n':
+                            print('=================================')
+                            print('Remoção cancelada.')
+                            print('=================================')
+                            break
+                        else:
+                            print('Digite s ou n')
+                    break
         
         elif opcao == 6:
             termo = ler_str('Qual documento deseja editar? ')
             resultados = buscar_documentos(documentos, termo)
             if not resultados:
+                print('=================================')
                 print('Nenhum documento encontrado.')
+                print('=================================')
                 continue
             else:
-                listar_documentos_com_indice(resultados)
-                indice = ler_int('Digite o número do documento que deseja editar: ')
-                if indice < 1 or indice > len(resultados):
-                    print('Número de documento inválido.')
-                    continue
-                else:
-                    doc_escolhido = resultados[indice - 1]
-                    nome_antes = doc_escolhido['nome']
-                    validade_antes = doc_escolhido['validade']
-                    alerta_antes = doc_escolhido['alerta']
-                    mostrar_menu_edicao()
-                    alterou = False
-                    dias_antes, status_antes = calcular_status(doc_escolhido)
-                    opcao_edicao = ler_int('Escolha uma opção: ')
-                    if opcao_edicao == 1:
-                        novo_nome = ler_str('Digite o novo nome: ')
-                        doc_escolhido['nome'] = novo_nome
-                        alterou = True
+                print('=================================')
+                resultados_ordenados = listar_documentos_com_indice(resultados)
+                print('=================================')
 
-                    elif opcao_edicao == 2:
-                        nova_validade = ler_data('Digite a nova data de validade: YYYY-MM-DD')
-                        doc_escolhido['validade'] = nova_validade
-                        alterou = True
-                    
-                    elif opcao_edicao == 3:
-                        while True:
-                            novo_alerta = ler_int('Digite os dias para o novo alerta: ')
-                            if novo_alerta < 0:
-                                print('Valor inválido (não pode ser negativo) ')
-                                continue
-                            doc_escolhido['alerta'] = novo_alerta
-                            alterou = True
-                            break
-
-                    elif opcao_edicao == 4:
-                        print('Cancelando...')
+                while True:
+                    indice = ler_int('Digite o número do documento que deseja editar: ')
+                    if indice < 1 or indice > len(resultados_ordenados):
+                        print('=================================')
+                        print('Número de documento inválido.')
+                        print('=================================')
                         continue
                     else:
-                        print('Opção inválida. Tente novamente.')
-                    if alterou:
-                        nome_editado = doc_escolhido['nome']
-                        dias_editado, status_editado = calcular_status(doc_escolhido)
-                        print('Alteração pronta. Confirme.')
-                        print('Antes')
-                        print(f"{nome_antes} | validade {validade_antes} | vence em {dias_antes} dia(s) | alerta para {alerta_antes} dia(s) | {status_antes}")
-                        print('Depois')
-                        print(f"{nome_editado} | validade {doc_escolhido['validade']} | vence em {dias_editado} dia(s) | alerta para {doc_escolhido['alerta']} dia(s) | {status_editado}")
+
                         while True:
-                            opcao_confirmacao = ler_str('Deseja salvar a alteração? (s/n)').strip().lower()
-                            if opcao_confirmacao == 's':
-                                salvar_documento(documentos, caminho)
-                                print('Salvo com sucesso.')
-                                break
-                            elif opcao_confirmacao == 'n':
-                                doc_escolhido['nome'] = nome_antes
-                                doc_escolhido['validade'] = validade_antes
-                                doc_escolhido['alerta'] = alerta_antes
-                                print('Alterações descartadas.')
+                            doc_escolhido = resultados_ordenados[indice - 1]
+                            nome_antes = doc_escolhido['nome']
+                            validade_antes = doc_escolhido['validade']
+                            alerta_antes = doc_escolhido['alerta']
+                            mostrar_menu_edicao()
+                            alterou = False
+                            dias_antes, status_antes = calcular_status(doc_escolhido)
+                            opcao_edicao = ler_int('Escolha uma opção: ')
+                            if opcao_edicao == 1:
+                                novo_nome = ler_str('Digite o novo nome: ')
+                                doc_escolhido['nome'] = novo_nome
+                                alterou = True
+
+                            elif opcao_edicao == 2:
+                                nova_validade = ler_data('Digite a nova data de validade: YYYY-MM-DD ')
+                                doc_escolhido['validade'] = nova_validade
+                                alterou = True
+                            
+                            elif opcao_edicao == 3:
+                                while True:
+                                    novo_alerta = ler_int('Digite os dias para o novo alerta: ')
+                                    if novo_alerta < 0:
+                                        print('=================================')
+                                        print('Valor inválido (não pode ser negativo) ')
+                                        print('=================================')
+                                        continue
+                                    doc_escolhido['alerta'] = novo_alerta
+                                    alterou = True
+                                    break
+
+                            elif opcao_edicao == 4:
+                                print('=================================')
+                                print('Cancelando...')
+                                print('=================================')
                                 break
                             else:
-                                print('Digite s ou n')
-
-
+                                print('=================================')
+                                print('Opção inválida. Tente novamente.')
+                                print('=================================')
+                            if alterou:
+                                nome_editado = doc_escolhido['nome']
+                                dias_editado, status_editado = calcular_status(doc_escolhido)
+                                print('=================================')
+                                print('Alteração pronta. Confirme.')
+                                print('Antes')
+                                if dias_antes < 0:
+                                    print(f'{nome_antes} | validade {validade_antes} | vencido há {-(dias_antes)} dia(s) | {status_antes}')
+                                else:    
+                                    print(f'{nome_antes} | validade {doc_escolhido['validade']} | vence em {dias_antes} dia(s) | alerta para {doc_escolhido['alerta']} | {status_antes}')
+                                print('Depois')
+                                print(f"{nome_editado} | validade {doc_escolhido['validade']} | vence em {dias_editado} dia(s) | alerta para {doc_escolhido['alerta']} dia(s) | {status_editado}")
+                                print('=================================')
+                                while True:
+                                    opcao_confirmacao = ler_str('Deseja salvar a alteração? (s/n)').strip().lower()
+                                    if opcao_confirmacao == 's':
+                                        salvar_documento(documentos, caminho)
+                                        print('=================================')
+                                        print('Salvo com sucesso.')
+                                        print('=================================')
+                                        break
+                                    elif opcao_confirmacao == 'n':
+                                        doc_escolhido['nome'] = nome_antes
+                                        doc_escolhido['validade'] = validade_antes
+                                        doc_escolhido['alerta'] = alerta_antes
+                                        print('=================================')
+                                        print('Alterações descartadas.')
+                                        print('=================================')
+                                        break
+                                    else:
+                                        print('Digite s ou n')
+                    break
 
 
 
@@ -281,7 +340,10 @@ def main() -> None:
             break
 
         else:
+            print('=================================')
             print('Opção inválida. Tente novamente.')
+            print('=================================')
+
 
 if __name__ == "__main__":
     main()
